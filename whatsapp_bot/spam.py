5026ee08c9d327aafc8108f672ee3db52665f2e8
shubham yadav <68185027+shubh22121@users.noreply.github.com>
2021-06-01 01:11:35 +0530

    Add files via upload

---
+++ whatsapp_bot/spam.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+import time
+
+chrome =webdriver.Chrome(ChromeDriverManager().install())
+
+chrome.get("https://web.whatsapp.com/")
+time.sleep(15)
+print("scan is complete")
+search_box = chrome.find_element_by_class_name("_2_1wd")
+search_box.send_keys("")
+search_box.send_keys(Keys.ENTER)
+
+'''message_box = chrome.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
+message_box.send_keys("hello")
+message_box.send_keys(Keys.ENTER)'''
+
+for i in range(0,10000):
+    message_box = chrome.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
+    message_box.send_keys("dont p")
+    message_box.send_keys(Keys.ENTER)
+
+    #  emoji=[":-)"]
